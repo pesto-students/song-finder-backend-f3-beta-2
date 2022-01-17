847dcd703876bdff4f934252bf0d16f9f6964b94
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-17 23:05:13 +0530

    access allow credentials set to true

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ require("dotenv/config");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use(express.json());
 
 // Connect to Database
