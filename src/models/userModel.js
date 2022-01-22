cced274fd4ca94bd92cf382641efd7733bd65f64
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-22 21:24:38 +0530

    add non strict mode

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    passwordHash: { type: String, required: true },
-    searchHistory: { type: Array, required: false }
-});
+const userSchema = new mongoose.Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        email: { type: String, required: true },
+        passwordHash: { type: String, required: true }
+    },
+    { strict: false }
+);
 
 exports.User = mongoose.model("user", userSchema);
