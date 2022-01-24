b7cb120bf78f83bfb0091ccf04d31054c16cd65c
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-24 19:40:21 +0530

    add host url from env

--- src/utils/sendEmail/sendEmail.js
+++ src/utils/sendEmail/sendEmail.js
@@ -14,7 +14,7 @@ async function SendEmail({ email, token, expiry }) {
         subject: "Immersis Password Reset",
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
             Please click on the following link, or paste this into your browser to complete the process:\n\n
-            http://${process.env.FRONT_SITE}/reset/${token}?expiry=${expiry}
+            http://${process.env.FRONT_SITE}/reset?token=${token}&expiry=${expiry}
             \n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`
     };
     await smtpTransport.sendMail(mailOptions);
